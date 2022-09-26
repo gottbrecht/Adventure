8b9821b9c74f5b6efcafd8431d8305f7534ebeab
gottbrecht <simo79j5@stud.kea.dk>
2022-09-26 02:27:08 -0700

    test2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello beautifuk people");
+        System.out.println("Hello beautiful people");
     }
 
 
